b750e542010135d399c3ef2ff25514420c79a2c3
desperado <desperado@DESKTOP-QE5CQNS>
2017-10-19 17:51:42 +0300

    complete

--- TableCorector/MainWindow.xaml.cs
+++ TableCorector/MainWindow.xaml.cs
@@ -1,17 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Linq;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+using System.IO;
 
 namespace TableCorector
 {
@@ -23,6 +16,84 @@ namespace TableCorector
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        string filepath = "test.docx";
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            DirectoryInfo di = new DirectoryInfo(".");
+            foreach (var file in di.GetFiles("*.docx"))
+                file.Delete();
+            DirectoryInfo di2 = new DirectoryInfo("../../../");
+            foreach (var file in di2.GetFiles("*.docx"))
+                file.CopyTo(di.FullName + "/" + file.Name);
+
+            using (WordprocessingDocument docWord = WordprocessingDocument.Open(filepath, true))
+            {
+                Table table1 = docWord.MainDocumentPart.Document.Body.Elements<Table>().First();
+                foreach (var table in docWord.MainDocumentPart.Document.Body.Elements<Table>())
+                {
+
+                    int i = 0;
+                    int check;
+                    foreach (var item in table.Elements<TableRow>())
+                    {
+                        if (i == 0)
+                        {
+                            shapkaCorrector(item);
+                            i++;
+                        }
+                        else
+                        {
+                            foreach (TableCell c in item.Elements<TableCell>())
+                            {
+                                if (Int32.TryParse(c.Elements<Paragraph>().First().Elements<Run>().First().Elements<Text>().First().Text, out check))
+                                {
+                                    ParagraphProperties pPr = new ParagraphProperties(new Justification() { Val = JustificationValues.Center });
+                                    c.PrependChild(pPr);
+                                }
+                                else
+                                {
+                                    ParagraphProperties pPr = new ParagraphProperties(new Justification() { Val = JustificationValues.Both });
+                                    c.PrependChild(pPr);
+                                }
+
+                                foreach (Paragraph para in c.Elements<Paragraph>())
+                                {
+                                    foreach (Run run in para.Elements<Run>())
+                                    {
+                                        RunProperties rPr = new RunProperties(new RunFonts() { Ascii = "Times New Roman" },
+                                                                      new FontSize() { Val = "24" });
+                                        run.PrependChild(rPr);
+                                    }
+                                }
+                            }
+
+                        }
+
+                    }
+                }
+            }
+
+        }
+        void shapkaCorrector(TableRow shapka)
+        {
+            foreach (TableCell cell in shapka.Elements<TableCell>())
+            {
+                ParagraphProperties pPr = new ParagraphProperties(new Justification() { Val = JustificationValues.Center });
+                cell.PrependChild(pPr);
+
+                foreach (Paragraph para in cell.Elements<Paragraph>())
+                {
+                    foreach (Run run in para.Elements<Run>())
+                    {
+                        RunProperties rPr = new RunProperties(new RunFonts() { Ascii = "Times New Roman" },
+                                                      new FontSize() { Val = "28" });
+                        run.PrependChild(rPr);
+                    }
+                }
+            }
         }
     }
 }
