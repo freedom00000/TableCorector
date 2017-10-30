de95fd46d268a36e7f02c07ea6f7887f3ad3db7e
desperado <desperado@DESKTOP-QE5CQNS>
2017-10-30 22:16:11 +0300

    nakolhozil

--- TableCorector/MainWindow.xaml.cs
+++ TableCorector/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Wordprocessing;
 
 using System.IO;
+using System.Collections.Generic;
 
 namespace TableCorector
 {
@@ -31,30 +32,42 @@ namespace TableCorector
 
             using (WordprocessingDocument docWord = WordprocessingDocument.Open(filepath, true))
             {
-                
-                foreach (var table in docWord.MainDocumentPart.Document.Body.Elements<Table>())
-                {
 
+                foreach (var table in docWord.MainDocumentPart.Document.Body.Elements<Table>())
+                { 
+                    List<List<string>> numCells = new List<List<string>>();
                     int i = 0;
-                    int check;
+                    double check;
                     foreach (var item in table.Elements<TableRow>())
                     {
+                        
                         if (i == 0)
                         {
                             shapkaCorrector(item);
                             i++;
                         }
                         else
-                        {
+                        {int j = 0;
+                            List<string> list = new List<string>();
+
                             foreach (TableCell c in item.Elements<TableCell>())
                             {
-                                if (Int32.TryParse(c.Elements<Paragraph>().First().Elements<Run>().First().Elements<Text>().First().Text, out check))
+                                
+                                if (Double.TryParse(c.Elements<Paragraph>().First().Elements<Run>().First().Elements<Text>().First().Text, out check))
                                 {
+                                    string st = "";
+                                    foreach(var str in c.Elements<Paragraph>().First().Elements<Run>().First().Elements<Text>())
+                                    {
+                                        st += str.Text;
+                                    }
+
+                                    list.Add(c.Elements<Paragraph>().First().Elements<Run>().First().Elements<Text>().First().Text);
                                     ParagraphProperties pPr = new ParagraphProperties(new Justification() { Val = JustificationValues.Center });
                                     c.PrependChild(pPr);
                                 }
                                 else
                                 {
+                                    list.Add(null);
                                     ParagraphProperties pPr = new ParagraphProperties(new Justification() { Val = JustificationValues.Both });
                                     c.PrependChild(pPr);
                                 }
@@ -66,13 +79,19 @@ namespace TableCorector
                                         RunProperties rPr = new RunProperties(new RunFonts() { Ascii = "Times New Roman" },
                                                                       new FontSize() { Val = "24" });
                                         run.PrependChild(rPr);
+
+
                                     }
                                 }
+                                j++;
                             }
-
+                            numCells.Add(list);
                         }
-
+                        
+                        
                     }
+                    numCorrector(numCells, table);
+                    i++;
                 }
             }
 
@@ -97,5 +116,76 @@ namespace TableCorector
                 }
             }
         }
+
+
+        void numCorrector(List<List<string>> list, Table table)
+        {
+            int count = 0;
+            for (int i = 0; i < list[1].Count; i++)
+            {
+                for (int j = 0; j < list.Count; j++)
+                {
+                    if (list[j][i] != null &&
+                        count < list[j][i].Split(',')[0].Count())
+                    {
+                        count = list[j][i].Split(',')[0].Count();
+                    }
+                }
+
+                for (int j = 0; j < list.Count; j++)
+                {
+                    if (list[j][i] != null)
+                    {
+                        list[j][i] = addSpace(count, list[j][i]);
+                    }
+                }
+            }
+
+            insertTable(table, list);
+        }
+
+        string addSpace(int count, string str)
+        {
+            int str_count = 0;
+
+            str_count = count - str.Split(',')[0].Count();
+            str = str.Insert(0, new string(Char.Parse("\u00A0"), str_count*2));
+         
+
+            return str;
+        }
+
+        void insertTable(Table table, List<List<string>> list) {
+            int j = -1;
+            int i = -1;
+            foreach (var item in table.Elements<TableRow>())
+            {
+                j = 0;
+                foreach (TableCell c in item.Elements<TableCell>())
+                    {
+
+                        foreach (Paragraph para in c.Elements<Paragraph>())
+                        {
+                        foreach (Run run in para.Elements<Run>())
+                        {
+
+                            foreach (var t in run.Elements<Text>())
+                            {
+                                if (i >= 0 && j >= 0  && j<4)
+                                if (list[i][j] != null)
+                                t.Text = list[i][j];
+                            }
+                             
+                             }
+                        }
+                    j++;
+                    }
+
+                i++;
+
+            }
+
+        }
+
     }
 }
